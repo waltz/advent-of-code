ab9353ef25f097bb465a58468d4ef2f7f65014d8
CJ Bryan <cj@cjbryan.io>
2024-12-06 23:12:46 -0800

    Struggling with slice appending

---
+++ 2024/day-2/part-2.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	// file, err := os.Open("input-1-test.txt")
+	if err != nil {
+		fmt.Println("There was a problem reading the file.")
+		return
+	}
+
+	safeReports := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		rawReport := strings.Split(scanner.Text(), " ")
+
+		report := []int{}
+
+		for _, rawLevel := range rawReport {
+			level, err := strconv.Atoi(rawLevel)
+
+			if err != nil {
+				fmt.Println("error parsing a level", level)
+				return
+			}
+
+			report = append(report, level)
+		}
+
+		fmt.Println(report)
+		isSafe := determineSafety(report)
+
+		// fmt.Println("initial safety?", isSafe)
+
+		if isSafe == false {
+			// fmt.Println("not safe, checking variants")
+			for i, _ := range report {
+				variant := []int{}
+				firstHalf := report[:i]
+				secondHalf := report[i+1:]
+				variant = append(variant, firstHalf...)
+				variant = append(variant, secondHalf...)
+
+				isVariantSafe := determineSafety(variant)
+
+				if isVariantSafe {
+					// fmt.Println("found a variant that is safe", variant)
+					isSafe = true
+					break
+				}
+			}
+		}
+
+		fmt.Println("final safety", isSafe)
+
+		if isSafe == true {
+			safeReports += 1
+		}
+	}
+
+	fmt.Println("finished, safe reports count:", safeReports)
+}
+
+func determineSafety(report []int) bool {
+	isSafe := true
+	isIncreasing := true
+
+	for i := 0; i < (len(report) - 1); i++ {
+		difference := report[i] - report[i+1]
+
+		// establish the trend on our first pass
+		if i == 0 && difference > 0 {
+			isIncreasing = false
+		}
+
+		// on successive passes, make sure we continue with the trend.
+		if i > 0 && difference > 0 && isIncreasing == true {
+			// fmt.Println("the difference is decreasing, but it should be increasing. unsafe.", difference, report[i], report[i+1])
+			isSafe = false
+			break
+		}
+
+		// same here, but if the trend is in the opposite direction
+		if i > 0 && difference < 0 && isIncreasing == false {
+			// fmt.Println("the difference is increasing, but it should be decreasing. unsafe.", difference, report[i], report[i+1])
+			isSafe = false
+			break
+		}
+
+		// make sure that the difference is never greater than three or less than 1
+		if difference < 0 {
+			difference = -difference
+		}
+
+		// fmt.Println(difference)
+		if difference < 1 || difference > 3 {
+			// fmt.Println("the difference was less than one or greater than three", difference)
+			isSafe = false
+			break
+		}
+	}
+
+	return isSafe
+}
